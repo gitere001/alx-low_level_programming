b5da123940315d0a449bdd410e33a7b68f1bafc6
james gitere <sasmuypwa2019@gmail.com>
2023-09-08 14:23:05 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 8-print_base16.c -o 8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - hex_digit
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char hex_digit;
+
+	for (hex_digit = '0'; hex_digit <= '9'; hex_digit++)
+	{
+	putchar(hex_digit);
+	}
+
+	for (hex_digit = 'a'; hex_digit <= 'f'; hex_digit++)
+	{
+	putchar(hex_digit);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
