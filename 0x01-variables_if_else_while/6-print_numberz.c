ac2d5c4efb07e16974a9e3d15acb7d27781f44e8
james gitere <sasmuypwa2019@gmail.com>
2023-09-08 14:08:27 -0700

    digits

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - digits
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+
+	{
+	putchar('0' + digit);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
