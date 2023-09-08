4af93c7a6865e4474f1838b8280548d5032a2677
james gitere <sasmuypwa2019@gmail.com>
2023-09-08 13:51:12 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - printing all base of 10
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int digit = 0;
+
+		while (digit <= 9)
+
+	{
+		printf("%d\n", digit);
+		digit++;
+	}
+
+	return (0);
+}
