717f894595b7b5c701f4df7dd4adee318c5776be
james gitere <sasmuypwa2019@gmail.com>
2023-09-18 10:08:46 -0700

    change value of n to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - takes a pointer to an int as parameter
+ * @n: pointer to the value to be changed
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
