8a3db3f33d9c55e95dd05056996302cbfee37f32
james gitere <sasmuypwa2019@gmail.com>
2023-09-18 12:23:39 -0700

    reversing a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,14 +10,14 @@ void rev_string(char *s)
 	int i;
 	int count = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0 ; s[i] != '\0' ; i++)
 		count++;
-	for (i = 0; i < count / 2; i++)
+	for (i = 0 ; i < count / 2 ; i++)
 	{
 		char j;
 
 		j = s[i];
 		s[i] = s[count - 1 - i];
-		s[count - 1 - 1] = j;
+		s[count - 1 - i] = j;
 	}
 }
