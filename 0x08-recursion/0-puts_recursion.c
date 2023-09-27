dc20b51125f9f16c8d0f99cb345b6f25c20f7d5e
james gitere <sasmuypwa2019@gmail.com>
2023-09-27 07:48:57 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function to print a string
+ * @s: function parameter
+ * Return: 0
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+	_putchar(*s);
+	_puts_recursion(s + 1);
+	}
+	else
+	_putchar('\n');
+
+
+}
