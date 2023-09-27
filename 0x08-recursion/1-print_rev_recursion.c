47f8e9c9b1db497ff298059d57d4d19d95e358eb
james gitere <sasmuypwa2019@gmail.com>
2023-09-27 08:17:18 -0700

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - function to print a string in rev
+ * @s: function parameter
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+	_putchar('\0');
+}
