bfb4f00eecfd1bdb3f51f33c87d89a40975c2f5a
james gitere <sasmuypwa2019@gmail.com>
2023-09-27 10:08:35 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - function that return the length
+ * @s: the string
+ * Return: length
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	return (0);
+}
