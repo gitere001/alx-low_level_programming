bbfc99cb68691255ac647dfe77bffad80efa9c46
gitere001 <sasmuypwa2019@gmail.com>
2023-10-19 10:56:33 -0700

    function that is executed before main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+void printText(void)__attribute__ ((constructor));
+/**
+ * printText - Prints the specified text to the console
+ */
+void printText(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
