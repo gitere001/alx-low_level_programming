9d9afc8bbe525b1eefaf7402b4a9268023aec927
gitere001 <sasmuypwa2019@gmail.com>
2023-10-19 11:02:14 -0700

    a 64-bit program in assembly that prints Hello, Holberton, followed by a new line.

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,13 @@
+section .data
+    format db "Hello, Holberton", 10, 0   ; Define the format string, including a newline character
+
+section .text
+    global main
+    extern printf
+
+main:
+    sub rsp, 8             ; Align the stack for printf
+    mov rdi, format        ; Load the address of the format string into rdi
+    call printf            ; Call the printf function
+    add rsp, 8             ; Restore the stack
+    ret
