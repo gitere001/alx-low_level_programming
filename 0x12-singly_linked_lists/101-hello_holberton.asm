10e0a43f29410ff4b18bdbfe2824f7d8f834c35d
gitere001 <sasmuypwa2019@gmail.com>
2023-10-19 22:28:21 -0700

    correction

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,9 @@
-section .data
-    hello db "Hello, Holberton", 10, 0 ; Define the format string, including a newline character
-
-section .text
-    global main
-    extern printf
-
+global main
+	extern printf
 main:
-    sub     rsp, 8             ; Align the stack for printf
-    mov     rdi, hello         ; Load the address of the format string into rdi
-    call    printf             ; Call the printf function
-    add     rsp, 8             ; Restore the stack
-    ret
+	mov edi, format
+	xor eax, eax
+	call printf
+	mov	eax, 0
+	ret
+format: db 'Hello, Holberton', 0
