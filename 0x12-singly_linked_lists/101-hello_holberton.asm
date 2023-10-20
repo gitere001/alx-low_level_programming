e541d3257448c38e561ec567d05859935fa3540c
gitere001 <sasmuypwa2019@gmail.com>
2023-10-19 22:34:44 -0700

    correction

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,13 @@
 global main
-	extern printf
+extern printf
+
+section .data
+    format db 'Hello, Holberton', 10, 0  ; The format string with newline and null terminator
+
+section .text
 main:
-	mov edi, format
-	xor eax, eax
-	call printf
-	mov	eax, 0
-	ret
-format: db 'Hello, Holberton', 0
+    mov edi, format
+    xor eax, eax
+    call printf
+    mov eax, 0
+    ret
