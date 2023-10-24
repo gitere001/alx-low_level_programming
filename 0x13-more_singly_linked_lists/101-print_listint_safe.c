e625243216b8a88909375dc08cf5d93d7e229358
gitere001 <sasmuypwa2019@gmail.com>
2023-10-24 07:14:36 -0700

    a function that prints a listint_t linked list.

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,72 @@
+#include "lists.h"
+
+/**
+ * free_list_safe - Frees a linked list.
+ * @head: Pointer to the head of the list.
+ *
+ * Description: This function frees a linked list of listint_t nodes.
+ */
+void free_list_safe(listp_t **head)
+{
+	listp_t *temp;
+	listp_t *curr;
+
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
+
+/**
+ * print_listint_safe - Prints a linked list safely.
+ * @head: Pointer to the head of the list.
+ *
+ * Description: This function prints a linked list of listint_t nodes
+ * while detecting and handling loops safely.
+ *
+ * Return: The number of nodes in the list.
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t nnodes = 0;
+	listp_t *hptr, *new, *add;
+
+	hptr = NULL;
+	while (head != NULL)
+	{
+		new = malloc(sizeof(listp_t));
+
+		if (new == NULL)
+			exit(98);
+
+		new->p = (void *)head;
+		new->next = hptr;
+		hptr = new;
+
+		add = hptr;
+
+		while (add->next != NULL)
+		{
+			add = add->next;
+			if (head == add->p)
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free_list_safe(&hptr);
+				return (nnodes);
+			}
+		}
+
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+		nnodes++;
+	}
+
+	free_list_safe(&hptr);
+	return (nnodes);
+}
